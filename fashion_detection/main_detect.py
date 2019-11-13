27c6222e98e45626f66ebeee4cdbb19f86e17476
wlaud1001 <wlaud1001@naver.com>
2019-11-13 21:06:08 +0900

    remove background for color

--- fashion_detection/main_detect.py
+++ fashion_detection/main_detect.py
@@ -1,5 +1,6 @@
 from upper_lower_detection import *
 from upper_pattern_classification import *
+from remove_background import *
 from PIL import Image
 import os, cv2
 from torchvision import transforms
@@ -7,7 +8,16 @@ import numpy as np
 
 #RGB
 def get_color(img):
+    idx = []
     data = np.reshape(img, (-1,3))
+    for i in range(data.shape[0]):
+        if np.all(data[i] == [0,0,0]):
+            idx.append(i)
+    idx.reverse()
+    data = data.tolist()
+    for i in idx:
+        data.pop(i)
+
     data = np.float32(data)
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
     flags = cv2.KMEANS_RANDOM_CENTERS
@@ -15,29 +25,30 @@ def get_color(img):
     return centers[0].astype(np.int32)[::-1]
 
 
-img_name = 'sss5'
+img_name = 'sss4'
 img_path = 'sample_images/'+img_name+'.jpg'
 
 img_path = os.path.join(img_path)
 img = Image.open(img_path).convert("RGB")
+
+back_removed_img = remove_background(img)
+
+cropping = upper_lower_detection(back_removed_img)
+
 trans = transforms.ToTensor()
 img = trans(img)
 img = img.mul(255).permute(1, 2, 0).byte().numpy()
 
 
-b, g, r = cv2.split(img)  # img파일을 b,g,r로 분리
-rimg = cv2.merge([r, g, b])
-
-
-cropping = upper_lower_detection(img)
 upper_categorise = [1,2]
 category_list = ['short_sleeve_top', 'long_sleeve_top','short_sleeve_outwear','long_sleeve_outwear','vest','sling','shorts','trousers','skirt','short_sleeve_dress','long_sleeve_dress','vest_dress','sling_dress' ]
-for idx in range(len(cropping)):
+for idx in range(2):
     cropped_img = cropping[idx]['cropping']
+    color = get_color(cropped_img)
     cropped_img = Image.fromarray(cropped_img)
     boxes = cropping[idx]['boxes']
     labels = cropping[idx]['label']
-    color = get_color(cropped_img)
+
 
     if cropping[idx]['label'] in upper_categorise:
         upper_pattern_pred = upper_pattern_detection(cropped_img)
@@ -51,12 +62,6 @@ for idx in range(len(cropping)):
 
     cv2.putText(img, name, (boxes[0], boxes[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (200, 200, 0), 2)
     cv2.putText(img, str(color), (boxes[0], boxes[1] - 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (200, 200, 0), 2)
-
-
-
-trans = transforms.ToTensor()
-img = trans(img)
-img = img.mul(255).permute(1, 2, 0).byte().numpy()
 
 
 b, g, r = cv2.split(img)  # img파일을 b,g,r로 분리
